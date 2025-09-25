bc43b3e8a80bca238c8f251c50449063c04cd5e5
Donal1999 <g00365724@atu.ie>
2025-09-25 21:28:18 +0100

    Request parameter added

--- src/main/java/ie/atu/week10/week2/RequestController.java
+++ src/main/java/ie/atu/week10/week2/RequestController.java
@@ -1,9 +1,6 @@
 package ie.atu.week10.week2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -18,5 +15,10 @@ public class RequestController {
     @GetMapping("/greet/{name}")// {name} is the PathVariable
     public String greetByname(@PathVariable String name){ // stores the name
         return "Hello "+ name +"!";
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam String name, @RequestParam int age){ //request parameters filters information
+        return "Name "+ name +"Age"+ age;
     }
 }
